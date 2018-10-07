38320414f57dbe41cee7db1aa52d398a7ffb3a82
root <ymbere@gmail.com>
2018-10-07 00:32:13 +0000

    Added source

---
+++ src/ListFavoritesMovies.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react'
+
+class ListFavoritesMovies extends Component {
+  render() {
+    return (
+      <div>
+      	<h2>this.props.movies.name</h2>
+        <p>Liked By:</p>
+        <ul>
+          <li>this.props</li>
+        </ul>
+      </div>
+    )
+  }
+  
+}
+
+export default ListFavoritesMovies
---
+++ src/ListMovies.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+import ListFavoriteMovies from './ListFavoritesMovies';
+
+class ListMovies extends Component { 
+  render() {
+   
+    return (
+      
+    )
+    
+  }
+}
+
+
+export default ListMovies
